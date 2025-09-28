648bc24c157f6c5975e0cbbe87bb4bf6a4b9d322
RingoI <miltonibarzabal5@gmail.com>
2025-09-28 16:19:32 -0300

    commit

--- src/redes_trabajos.js
+++ src/redes_trabajos.js
@@ -13,7 +13,7 @@ const TRABAJOS = [
     fecha: '28 - Oct - 2022',
     descripcion:
       'Game where you have to drag and drop to finish assembling the puzzle.',
-    website: 'https://jaenfigueroa.github.io/Rompecabezas/',
+    
     github: 'https://github.com/jaenfigueroa/Rompecabezas',
     imagen: './assets/proyectos/puzzle_shuerk.png'
   },
@@ -22,7 +22,7 @@ const TRABAJOS = [
     fecha: '1 - Nov - 2022',
     descripcion:
       'A personal project for halloween, an animation using only html and css.',
-    website: 'https://jaenfigueroa.github.io/Halloween-Pumpkin/',
+    
     github: 'https://github.com/jaenfigueroa/Halloween-Pumpkin',
     imagen: './assets/proyectos/halloween_pumpkin.png'
   },
@@ -31,7 +31,7 @@ const TRABAJOS = [
     fecha: '18 - Nov - 2022',
     descripcion:
       "A personal project, an animation of a rubik's cube in 3d using only html and css.",
-    website: 'https://jaenfigueroa.github.io/3D-Rubiks-Cube/',
+    
     github: 'https://github.com/jaenfigueroa/3D-Rubiks-Cube',
     imagen: './assets/proyectos/cubo_rubik.png'
   },
@@ -40,7 +40,7 @@ const TRABAJOS = [
     fecha: '16 - Nov - 2022',
     descripcion:
       'Minecraft game made only with CSS and Javascript, where you can move the character with the keys, change the person, etc.',
-    website: 'https://jaenfigueroa.github.io/JaenCraft/',
+    
     github: 'https://github.com/jaenfigueroa/JaenCraft',
     imagen: './assets/proyectos/jaencraft.png'
   },
@@ -49,7 +49,7 @@ const TRABAJOS = [
     fecha: '03 - Dec - 2022',
     descripcion:
       'It is used to take the remaining time, where you can set the minutes and seconds in which an alarm should be displayed.',
-    website: 'https://jaenfigueroa.github.io/Pomodoro-timer',
+    
     github: 'https://github.com/jaenfigueroa/Pomodoro-timer',
     imagen: './assets/proyectos/pomodoro_timer.png'
   },
@@ -58,7 +58,7 @@ const TRABAJOS = [
     fecha: '10 - Dec - 2022',
     descripcion:
       'Website where different food dishes of different prices and flavors are sold.',
-    website: 'https://jaenfigueroa.github.io/eCommerce',
+    
     github: 'https://github.com/jaenfigueroa/eCommerce',
     imagen: './assets/proyectos/ecommerce.png'
   },
@@ -67,7 +67,7 @@ const TRABAJOS = [
     fecha: '15 - Dec - 2022',
     descripcion:
       'Website where different food dishes of different prices and flavors are sold.',
-    website: 'https://jaenfigueroa.github.io/web-portfolio',
+    
     github: 'https://github.com/jaenfigueroa/web-portfolio',
     imagen: './assets/proyectos/web_portfolio.png'
   },
@@ -76,7 +76,7 @@ const TRABAJOS = [
     fecha: '15 - Dec - 2022',
     descripcion:
       'Application to save movie lists, you can edit them, move them, delete them, search them, etc.',
-    website: 'https://manage-movie-list.netlify.app/',
+    
     github: 'https://github.com/jaenfigueroa/manage-movie-list',
     imagen: './assets/proyectos/movies.png'
   },
@@ -85,7 +85,7 @@ const TRABAJOS = [
     fecha: '3 - Jan - 2023',
     descripcion:
       'This project is a Pokémon-themed website created with React.js and the PokeAPI API.',
-    website: 'https://pokezone.jaenfigueroa.com/',
+    
     github: 'https://github.com/jaenfigueroa/PokeZone',
     imagen: './assets/proyectos/pokezone.png'
   }
